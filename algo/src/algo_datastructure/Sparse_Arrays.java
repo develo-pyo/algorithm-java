d4b3de5c7efc90a6541ce121f4b42f799335174f
leejungpyo <jungpyo9@gmaill.com>
2019-11-20 17:09:56 +0900

    191120 algorithm

---
+++ algo/src/algo_datastructure/Sparse_Arrays.java
@@ -0,0 +1,28 @@
+package algo_datastructure;
+
+public class Sparse_Arrays {
+	
+	public static void main(String[] args) {
+		String[] str = {"aba", "baba", "aba", "xzxb"};	//비교대상문자열
+		String[] query = {"aba", "xzxb", "ab"};	//기준문자열
+		
+		int[] result = new int[query.length];
+		int i = 0;
+		for(String q : query) {
+			int tmp = 0;
+			for(String s : str) {
+				if(s.equals(q)) {
+					tmp++;
+				}
+			}
+			result[i] = tmp;
+			i++;
+		}
+		
+		
+		for(int r : result) {
+			System.out.print(r + " ");
+		}
+		
+	}
+}
