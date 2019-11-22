9f3361b7d5be861e7e6e266674274ad485ec24cb
leejungpyo <jungpyo9@gmaill.com>
2019-11-22 10:37:50 +0900

    배열 내 값들 이동시키기

--- algo/src/algo_datastructure/DynamicArray.java
+++ algo/src/algo_datastructure/DynamicArray.java
@@ -3,33 +3,24 @@ package algo_datastructure;
 public class DynamicArray {
 	
 	public static void main(String args[]) {
-//		
-//		int[] arr = {1, 2, 3, 4, 5};
-//		int l = arr.length;
-//		int move = 3;
-//		int m = (int)l%move;
-//		
-//		int[] r = new int[l];
-//		
-////		l 5 5 5 5 5
-////		m 2 2 2 2 2
-////		i 0 1 2 3 4
-////		  3 4 5 6 7 (l-m+i) 
-////		  3 4 0 1 2 (l-m+i)%l
-////		r 3 4 0 1 2
-//		
-//		for(int i=0; i<l; i++) {
-//			r[i] = arr[(l-m+i)%l];
-//			System.out.print(r[i]);
-//		}
 		
-		int[] arr = new int[3];
-		arr[1] = 4;
-		arr[2] = 5;
-		arr[0] = 2;
+		int[] arr = {1, 2, 3, 4, 5};
+		int l = arr.length;
+		int move = 3;
+		int m = (int)l%move;
 		
-		for(int i:arr) {
-			System.out.println(i + " ");
+		int[] r = new int[l];
+		
+//		l 5 5 5 5 5
+//		m 2 2 2 2 2
+//		i 0 1 2 3 4
+//		  3 4 5 6 7 (l-m+i) 
+//		  3 4 0 1 2 (l-m+i)%l
+//		r 3 4 0 1 2
+		
+		for(int i=0; i<l; i++) {
+			r[i] = arr[(l-m+i)%l];
+			System.out.print(r[i] + " ");
 		}
 		
 	}
