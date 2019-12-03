ac22694bac249d96964fde98b7e68431aa722554
develo-pyo <jungpyo9@gmail.com>
2019-12-03 11:03:16 +0900

    프로그래머스 stack 탑 문제

--- algo/src/algo/StrongPassword.java
+++ algo/src/algo/StrongPassword.java
@@ -5,52 +5,64 @@ import java.util.regex.Pattern;
 
 public class StrongPassword {
 	
+//	public static void main(String[] args) {
+//		
+//		String input = "Ad";
+//		
+////		String numbers = "0123456789";
+////		String lower_case = "abcdefghijklmnopqrstuvwxyz";
+////		String upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+////		String special_characters = "!@#$%^&*()-+";
+//		
+//		int needs = 0;
+//		
+//		Pattern p = Pattern.compile("[0-9]");
+//		
+//		Matcher m = p.matcher(input);
+//		if(!m.find()) {
+//			System.out.println("no");
+//			needs++;
+//		}
+//		
+//		Pattern p1 = Pattern.compile("[a-z]");
+//		m = p1.matcher(input);
+//		if(!m.find()) {
+//			System.out.println("lower ");
+//			needs++;
+//		}
+//		
+//		Pattern p2 = Pattern.compile("[A-Z]");
+//		m = p2.matcher(input);
+//		if(!m.find()) {
+//			System.out.println("capital");
+//			needs++;
+//		}
+//		
+//		Pattern p3 = Pattern.compile("[!@#$%^&*()\\-+]");
+//		m = p3.matcher(input);
+//		if(!m.find()) {
+//			System.out.println("special");
+//			needs++;
+//		}
+//		
+//		System.out.println("needs:"+needs);
+//		
+//		
+//		if(6 > input.length()+needs) {
+//			needs = 6-input.length();
+//		}
+//		
+//		System.out.println(needs);
+//	}
+	
 	public static void main(String[] args) {
 		
-		String input = "#HackerRank";
-		
-//		String numbers = "0123456789";
-//		String lower_case = "abcdefghijklmnopqrstuvwxyz";
-//		String upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String special_characters = "!@#$%^&*()-+";
-		
-		int needs = 0;
+		String input = "12";
+		System.out.println(Pattern.matches("[0-9]+", input));
 		
 		Pattern p = Pattern.compile("[0-9]+");
 		Matcher m = p.matcher(input);
-		if(!m.find()) {
-			System.out.println("no");
-			needs++;
-		}
+		System.out.println(m.find());
 		
-		Pattern p1 = Pattern.compile("[a-z]+");
-		m = p1.matcher(input);
-		if(!m.find()) {
-			System.out.println("lower ");
-			needs++;
-		}
-		
-		Pattern p2 = Pattern.compile("[A-Z]+");
-		m = p2.matcher(input);
-		if(!m.find()) {
-			System.out.println("capital");
-			needs++;
-		}
-		
-		Pattern p3 = Pattern.compile("[!@#$%^&*()\\-+]+");
-		m = p3.matcher(input);
-		if(!m.find()) {
-			System.out.println("special");
-			needs++;
-		}
-		
-		System.out.println("needs:"+needs);
-		
-		
-		if(6 > input.length()+needs) {
-			needs = 6-input.length();
-		}
-		
-		System.out.println(needs);
 	}
 }
---
+++ algo/src/algo_datastructure/Programmers_Tower.java
@@ -0,0 +1,25 @@
+package algo_datastructure;
+
+public class Programmers_Tower {
+	
+	public static void main(String[] args) {
+//		int[] tower = {6, 9, 5, 7, 4};
+		int[] tower = {3, 9, 9, 3, 5, 7, 2};
+		
+		int[] answer = new int[tower.length];
+		
+		for(int i=1; i<tower.length; i++) {
+			for(int j=i; j>=0; --j) {
+				if(tower[j] > tower[i]) {
+					answer[i] = j+1;
+					break;
+				}
+			}
+		}
+		
+		for(int i : answer) {
+			System.out.print(i + " ");
+		}
+	}
+	
+}
