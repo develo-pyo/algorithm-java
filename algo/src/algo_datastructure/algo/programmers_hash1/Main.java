d47762ffa1b4f8d32ee4b438d568eff71b8c5f5a
develo-pyo <jungpyo9@gmail.com>
2019-12-05 11:01:20 +0900

    프로그래머스 해시 완주하지못한선수

--- algo/src/algo_datastructure/algo/programmers_hash1/Main.java
+++ algo/src/algo_datastructure/algo/programmers_hash1/Main.java
@@ -1,34 +1,54 @@
 package algo_datastructure.algo.programmers_hash1;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
-
+	//프로그래머스 > 해시 > 완주하지 못한 선수 
 	public static void main(String[] args) {
-		String[] part = {"leo", "kiki", "eden"};
-		String[] comp = {"kiki", "eden"};
+		String[] part = {"a", "b", "b", "c"};
+		String[] comp = {"a", "b", "b"};
 		System.out.println(solu(part, comp));
 	}
 	
+	//나의 풀이
 	private static String solu(String[] part, String[] comp) {
 		String result = "";
 		Arrays.sort(part);
 		Arrays.sort(comp);
-//		part : a b c
-//		comp : a b
+		print(comp);
+		print(comp);
 		
-		for(String p : part) {
-			boolean flag = false;
-			for(String c : comp) {
-				if(p.equalsIgnoreCase(c)) {
-					flag = true;
-				}
-			}
-			if(!flag) {
-				result = p;
+		for(int i=0; i<comp.length; i++) {
+			if(!part[i].equalsIgnoreCase(comp[i])){
+				result = part[i];
 			}
 		}
 		
+		if(result.isEmpty())result=part[part.length-1];
 		return result;
+	}
+	
+	//고수의 풀이
+	private static String solu2(String[] part, String[] comp) {
+		Arrays.sort(part);
+		Arrays.sort(comp);
+		print(comp);
+		print(comp);
+		
+		int i=0;	//for 문의 index 를 밖으로 빼서 사용..
+		for(i=0; i<comp.length; i++) {
+			if(!part[i].equalsIgnoreCase(comp[i])){
+				return part[i];
+			}
+		}
+		return part[i];
+	}
+	
+	private static void print(String[] input) {
+		for(String i : input) {
+			System.out.print(i +" ");
+		}
+		System.out.println();
 	}
 }
