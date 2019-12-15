076938787a9a207069fe286806eb252307908385
develo-pyo <jungpyo9@gmail.com>
2019-12-15 23:44:43 +0900

    hackerrank

--- algo/src/algo_hackerrank/HackerrankInAString.java
+++ algo/src/algo_hackerrank/HackerrankInAString.java
@@ -1,7 +1,9 @@
 package algo_hackerrank;
 
-public class HackerrankInAString {
+import java.util.Stack;
 
+public class HackerrankInAString {
+	//hackerrank in a String! : easy
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -15,10 +17,11 @@ public class HackerrankInAString {
 		int tmp = 0;
 		
 		for(int j=0; j<hackerrank.length(); j++) {
+			int b = 0;
 			for(int i=0; i<input.length(); i++) {
 				if(hackerrank.charAt(j) == input.charAt(i)) {
 					tmp++;
-					
+					b++;
 					input = input.substring(i+1);
 					System.out.println("input:"+input);
 					System.out.println("hackerrank:"+hackerrank);
@@ -26,11 +29,42 @@ public class HackerrankInAString {
 					break;
 				}
 			}
-			if(hackerrank.length() == tmp)return "YES";
+			if(hackerrank.length() == tmp) {
+				return "YES";
+			} 
+			if(b == 0) {
+				return "NO";
+			}
 		}
 		
 		return result;
 	}
 	
+	//using Stack
+	static String solu2(String s) {
+        // Complete this function
+        Stack<Character> stack= new Stack<>();
+        stack.push('k');
+        stack.push('n');
+        stack.push('a');
+        stack.push('r');
+        stack.push('r');
+        stack.push('e');
+        stack.push('k');
+        stack.push('c');
+        stack.push('a');
+        stack.push('h');
+        
+        String result="NO";
+        for(Character c:s.toCharArray()){
+            if(c==stack.peek())
+                stack.pop();
+            if(stack.isEmpty()){
+                result="YES";
+                break;
+            } 
+        }
+        return result;
+    }
 	
 }
