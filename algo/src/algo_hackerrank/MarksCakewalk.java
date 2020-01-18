ade24c740495311a72df9f0161bcabb1e2065634
develo-pyo <jungpyo9@gmail.com>
2020-01-18 22:33:24 +0900

    hackerrank easy

---
+++ algo/src/algo_hackerrank/MarksCakewalk.java
@@ -0,0 +1,24 @@
+package algo_hackerrank;
+
+import java.util.Arrays;
+
+public class MarksCakewalk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] arr = {7, 4, 9, 6};
+		long rs = marcsCakewalk(arr);
+		System.out.println("result:"+rs);
+	}
+	
+	static long marcsCakewalk(Integer[] calorie) {
+		long rs = 0;
+		
+		Arrays.sort(calorie, (x,y)->y-x);
+		for(int i=0; i<calorie.length; i++){
+			rs += Math.pow(2, i) * calorie[i];
+		}
+		
+		return rs;
+    }
+}
